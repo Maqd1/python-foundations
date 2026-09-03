5e56bc3bd677d40690d97281d74b0edf26b66c5c
Maqd <abdulqudusdamilola@gmail.com>
2026-09-03 17:50:17 +0100

    chore: add Git tracking comment

--- level-01/autobio.py
+++ level-01/autobio.py
@@ -11,3 +11,7 @@ birth_year =int(input("enter your birth_year:"))
 
 age = 2026 - birth_year
 print(f"Your name is {first_name} {last_name} and you are {age} years old.")
+
+
+
+# Git is tracking this file.
